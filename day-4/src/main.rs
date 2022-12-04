b0e43f7d8e312a586df0c17df4d604c45a407de3
Dov Alperin <dov@fly.io>
2022-12-04 12:29:45 -0500

    Day 4

---
+++ day-4/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./input.txt").unwrap();
+    let lines: Vec<_> = input.split_terminator("\n").collect();
+    let pairs: Vec<Vec<Vec<_>>> = lines
+        .iter()
+        .map(|line| {
+            line.split(",")
+                .map(|item| {
+                    item.split("-")
+                        .map(|item| item.parse::<u32>().unwrap())
+                        .collect()
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut problem_1 = 0;
+    let mut problem_2 = 0;
+    for pair in pairs {
+        let a = &pair[0];
+        let b = &pair[1];
+
+        if (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]) {
+            problem_1 = problem_1 + 1;
+        }
+
+        let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
+        let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
+        if a_range.iter().any(|i| b_range.contains(i)) {
+            problem_2 = problem_2 + 1;
+        }
+    }
+    println!("{}\n{}", problem_1, problem_2)
+}
