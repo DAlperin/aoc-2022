a1fff7a5920df2e97db45e82e904e7ed96ef9214
Dov Alperin <dov@fly.io>
2022-12-04 17:19:53 -0500

    Clean up day 4 a bit

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -18,16 +18,17 @@ fn main() {
 
     let mut problem_1 = 0;
     let mut problem_2 = 0;
+
     for pair in pairs {
         let a = &pair[0];
         let b = &pair[1];
+        let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
+        let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
 
         if (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]) {
             problem_1 = problem_1 + 1;
         }
 
-        let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
-        let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
         if a_range.iter().any(|i| b_range.contains(i)) {
             problem_2 = problem_2 + 1;
         }
