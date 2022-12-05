81c7db0b416c16d7eb509772610b3ad0c48cec03
Dov Alperin <dov@fly.io>
2022-12-04 23:46:52 -0500

    Pull in itertools for day 4

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,27 +1,33 @@
+use itertools::Itertools;
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
     let lines: Vec<_> = input.split_terminator("\n").collect();
-    let pairs: Vec<Vec<Vec<_>>> = lines
+    let pairs = lines
         .iter()
         .map(|line| {
             line.split(",")
                 .map(|item| {
-                    item.split("-")
+                    return match item
+                        .split("-")
                         .map(|item| item.parse::<u32>().unwrap())
-                        .collect()
+                        .collect_tuple()
+                    {
+                        Some(val) => val,
+                        None => (0, 0),
+                    };
                 })
-                .collect()
+                .collect_vec()
         })
-        .collect();
+        .collect_vec();
 
     let part_1: Vec<_> = pairs
         .iter()
         .filter(|pair| {
             let a = &pair[0];
             let b = &pair[1];
-            (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1])
+            (a.0 >= b.0 && a.1 <= b.1) || (b.0 >= a.0 && b.1 <= a.1)
         })
         .collect();
 
@@ -30,8 +36,8 @@ fn main() {
         .filter(|pair| {
             let a = &pair[0];
             let b = &pair[1];
-            let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
-            let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
+            let a_range: Vec<_> = (a.0..a.1 + 1).collect();
+            let b_range: Vec<_> = (b.0..b.1 + 1).collect();
             a_range.iter().any(|i| b_range.contains(i))
         })
         .collect();
