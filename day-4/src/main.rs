c72a7d4fc3bd78a1f0cda9b621fa0b0f6ca4debf
Dov Alperin <dov@fly.io>
2022-12-04 23:28:02 -0500

    Make day 4 more rusty

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -16,22 +16,25 @@ fn main() {
         })
         .collect();
 
-    let mut problem_1 = 0;
-    let mut problem_2 = 0;
+    let part_1: Vec<_> = pairs
+        .iter()
+        .filter(|pair| {
+            let a = &pair[0];
+            let b = &pair[1];
+            (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1])
+        })
+        .collect();
 
-    for pair in pairs {
-        let a = &pair[0];
-        let b = &pair[1];
-        let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
-        let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
+    let part_2: Vec<_> = pairs
+        .iter()
+        .filter(|pair| {
+            let a = &pair[0];
+            let b = &pair[1];
+            let a_range: Vec<_> = (a[0]..a[1] + 1).collect();
+            let b_range: Vec<_> = (b[0]..b[1] + 1).collect();
+            a_range.iter().any(|i| b_range.contains(i))
+        })
+        .collect();
 
-        if (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]) {
-            problem_1 = problem_1 + 1;
-        }
-
-        if a_range.iter().any(|i| b_range.contains(i)) {
-            problem_2 = problem_2 + 1;
-        }
-    }
-    println!("{}\n{}", problem_1, problem_2)
+    println!("{}\n{}", part_1.len(), part_2.len());
 }
